309dda89b2106543457a7690d6ae6ccfffaaf757
Divye Darshiny <it234008@saranathan.ac.in>
2023-10-24 19:22:27 +0530

    .

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const skills = ['Java', 'Python', 'Javascript', 'HTML & CSS', 'C'];
-
-const skillsList = document.getElementById('skills-list');
-
-skills.forEach(skill => {
-    const skillItem = document.createElement('li');
-    skillItem.textContent = skill;
-    skillsList.appendChild(skillItem);
-});
-
-$(function() {
+$(document).ready(function() {
+  const skills = ['Programming Languages: Java, C, Python', 'Web Development: HTML, CSS, Bootstrap, JavaScript', 'Database Management: PostgreSQL', 'Version Control: Git', 'IDEs: Visual Studio Code, IntelliJ IDEA, PyCharm'];
+  
+  const skillsList = document.getElementById('skills-list');
+  
+  skills.forEach(skill => {
+      const skillItem = document.createElement('li');
+      skillItem.textContent = skill;
+      skillsList.appendChild(skillItem);
+  });
+  
   $(document).scroll(function(){
-    var $nav = $("#mainNavbar");
-    $nav.toggleClass("scrolled",$(this).scrollTop() > $nav.height());
+      var $nav = $("#mainNavbar");
+      $nav.toggleClass("scrolled",$(this).scrollTop() > $nav.height());
   });
 });
