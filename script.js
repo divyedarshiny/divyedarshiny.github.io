3249718d4e8670bf5e869c90594b13811f597d45
Divye Darshiny S <117534861+divyedarshiny@users.noreply.github.com>
2023-10-27 04:38:41 +0000

    update

--- script.js
+++ script.js
@@ -8,13 +8,31 @@ $(document).ready(function() {
       skillItem.textContent = skill;
       skillsList.appendChild(skillItem);
   });
-
-  projects.forEach(project => {
-    const projectItem = document.createElement('li');
-    projectItem.textContent = project;
-    projectsList.appendChild(projectItem);
-});
-
+  $(document).ready(function() {
+    const projects = [
+      {
+        title: 'GenTech-Chronicle',
+        description: 'GenTech Chronicle is a blog application developed using Django, designed for readers to explore articles on various topics, including general and technical content. Readers can access and read blogs authored by the dedicated writer. This application provides a user-friendly interface to navigate and enjoy the provided content.',
+      },
+      {
+        title: 'CybeRover',
+        description: 'CybeRover is a dynamic chatbot application developed using Java and Spring Boot.',
+      },
+      {
+        title: 'Personal portfolio website',
+        description: 'Created a personal portfolio website using HTML, CSS, and JavaScript to showcase my skills, projects, and professional background.',
+      },
+    ];
+  
+    const projectsList = document.getElementById('projects-list');
+  
+    projects.forEach(project => {
+      const projectItem = document.createElement('li');
+      projectItem.innerHTML = `<strong>${project.title}</strong>: ${project.description}`;
+      projectsList.appendChild(projectItem);
+    });
+  });
+  
   
   $(document).scroll(function(){
       var $nav = $("#mainNavbar");
